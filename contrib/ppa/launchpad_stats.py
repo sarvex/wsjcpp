eae1c53b5ea9b62f3240a670fa505baf67886310
Sourcery AI <>
2023-05-19 06:13:40 +0000

    'Refactored by Sourcery'

--- contrib/ppa/launchpad_stats.py
+++ contrib/ppa/launchpad_stats.py
@@ -19,8 +19,10 @@ for bin in bins:
     count = bin.getDownloadCount()
     total += count
     if (count > 0):
-        builds.append([count,'%s %s' % (bin.binary_package_name,bin.binary_package_version)])
+        builds.append(
+            [count, f'{bin.binary_package_name} {bin.binary_package_version}']
+        )
 
 builds_sorted = sorted(builds,key=lambda count: count[0],reverse=True)
 for build in builds_sorted:
-   print(str(build[0]) + ":" + str(build[1]))
+    print(f"{str(build[0])}:{str(build[1])}")
